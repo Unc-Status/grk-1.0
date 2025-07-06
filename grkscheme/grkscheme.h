9520a62976215709511ee8f639151480ac39b892
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-06 16:48:55 -0500

    Update grkscheme.h

--- grkscheme/grkscheme.h
+++ grkscheme/grkscheme.h
@@ -1,1 +1,25 @@
+//grkscheme.h copyright ( c ) UncStatus++
 
+#ifndef GRK_H || GRK && GRK_SCHEME
+#define GRK_H || GRK && GRK_SCHEME
+
+//scheme types
+#define GRK_SCHEME_LIGHT
+#define GRK_SCHEME_DARK
+
+class GrkScheme{
+public :
+ GrkScheme();
+ virtual ~GrkScheme();
+
+ //declare theme type
+ int GrkSchemeType;
+ int GrkSchemeFlags;
+
+ //set
+ bool GrkSchemeSet;
+
+//end
+};
+
+#endif
