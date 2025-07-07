0bc920e521cf18577a696fa5c621b81f18b0375d
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-07 09:33:38 -0500

    Update grkframe.h

--- grk/grkframe.h
+++ grk/grkframe.h
@@ -6,6 +6,20 @@
 #define GRKFRAME_X
 #define GRKFRAME_Y
 
+class GrkFrame{
+public:
+ GrkFrame();
+ virtual ~GrkFrame();
+
+ int x,y;
+ int width, height;
+
+ int FrameScheme;
+
+ void GrkResizeFrame(GrkFrame * pFrame, int _x, int _y, bool bSized);
+
+ bool resizeable;
+};
 
 
 #endif
