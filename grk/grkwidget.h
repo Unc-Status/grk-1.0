39162abb858d976908ddfdc8c68d82fb9c7c7c06
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-07 19:47:46 -0500

    Create grkwidget.h

---
+++ grk/grkwidget.h
@@ -0,0 +1,19 @@
+//grkwidget.h copyright ( c ) UncStatus++
+//praise be to the lord
+
+#ifndef GRKWIDGET_H
+#define GRKWIDGET_H
+
+class GrkWidget{
+public:
+ GrkWidget();
+ virtual ~GrkWidget();
+
+ int x, y;
+ int width, height;
+
+ bool bgrkresizeable;
+
+};
+
+#endif
